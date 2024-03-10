97d1675967864f93f4483eb15263b5abb0a7a42e
45erika <161070032+45erika@users.noreply.github.com>
2024-03-09 19:55:37 -0600

    agrege hospitalizacion

--- Clases/VeterinariaC.cs
+++ Clases/VeterinariaC.cs
@@ -21,16 +21,16 @@ namespace Veterinaria.Clases
 
         [JsonProperty]
         Mascota[] mascotas;
-        
+
         [JsonProperty]
         Dueno[] duenos;
-        
+
         [JsonProperty]
         Usuario[] usuarios;
 
         static string rutaArchivo = @"VeterinariaC/RegistroVeterinaria.json";
-        
-        
+
+
 
         // Estatica (no necesito que la clase este instanciada)
         //      new VeterinariaC() <--- Esto no es necesario
@@ -43,7 +43,7 @@ namespace Veterinaria.Clases
         //      instanciaC.miAtributo
 
         // Metodo constructor sin parametros 
-        public VeterinariaC() {}
+        public VeterinariaC() { }
 
         // Metodo constructor con parametros
         public VeterinariaC(
@@ -62,7 +62,7 @@ namespace Veterinaria.Clases
         }
 
         public static VeterinariaC obtenerVeterinaria()
-        {            
+        {
             try
             {
                 //2. Obtener el archivo serializado
@@ -86,7 +86,7 @@ namespace Veterinaria.Clases
 
                 //5. Retorno una la instancia que ya cree de VeterinariaC
                 return instanciaVeterinariaC;
-            }                 
+            }
         }
 
         // Hoy
@@ -113,13 +113,31 @@ namespace Veterinaria.Clases
             }
 
             Consulta[] nuevaConsulta = new Consulta[1] { consulta };
+
             Consulta[] combiArregloConsulta = this.consultas.Concat(nuevaConsulta).ToArray();
+
             consultas = combiArregloConsulta;
 
             this.guardarAtributosVeterinaria();
         }
 
-        
+        public void registroHospitalizacion(Hospitalizacion hospitalizacion)
+        {
+            if(this.hospitalizaciones == null)
+            {
+                this.hospitalizaciones = new Hospitalizacion[0];
+            }
+            Hospitalizacion[] nuevaHospitalizacion = new Hospitalizacion[1] { hospitalizacion };
+
+            Hospitalizacion[] combiArregloHospitalizacion = this.hospitalizaciones.Concat(nuevaHospitalizacion).ToArray();
+
+            hospitalizaciones = combiArregloHospitalizacion;
+
+            this.guardarAtributosVeterinaria();
+        }
+
+
+
         public void agregarMascota(Mascota mascota)
         {
             if (this.mascotas == null)
